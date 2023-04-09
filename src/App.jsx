137190d48328a57d5ba125f46dae12777a39b584
aditya-pushkar <adityapushkar850@gmail.com>
2023-04-10 02:57:20 +0530

    Navbar --added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 
-import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Home/>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,109 @@
+import React, {useState} from "react";
+
+const UserProfileMenu = () => {
+  return (
+    <div className="flex justify-end -mt-2">
+<div
+    className="z-50 my-4 w-56  mx-2 text-base list-none bg-white rounded divide-y divide-gray-100 shadow "
+    id="dropdown"
+  >
+    <div className="py-3 px-4">
+      <span className="block text-sm font-semibold text-gray-900">
+        Neil sims
+      </span>
+      <span className="block text-sm font-light text-gray-700 truncate">
+        adi@grammeai.com
+      </span>
+    </div>
+    <ul
+      className="py-1 font-light text-gray-500 dark:text-gray-400"
+      aria-labelledby="dropdown"
+    >
+      <li>
+        <a
+          href="#"
+          className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
+        >
+          My profile
+        </a>
+      </li>
+      
+    </ul>
+    
+    <ul
+      className="py-1 font-light text-gray-500 dark:text-gray-400"
+      aria-labelledby="dropdown"
+    >
+      <li>
+        <a
+          href="#"
+          className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
+        >
+          Sign out
+        </a>
+      </li>
+    </ul>
+  </div>
+    </div>
+    
+  )
+};
+const Navbar = () => {
+    const [showUserProfileMenu, setShowUserProfileMenu]  = useState(false)
+
+    const handleUserProfileMenu = () => {
+        if(showUserProfileMenu){
+            return setShowUserProfileMenu(false)
+        }
+        return setShowUserProfileMenu(true)
+    }
+  return (
+    <div>
+      <header>
+        <nav className="w-screen bg-white border-gray-200 px-4 lg:px-6 py-2.5 sticky">
+          <div className="flex flex-wrap justify-between items-center">
+            <div className="flex justify-start items-center mb-3 md:mb-0 -gap-3">
+              <a href="#" className="flex mr-4  ">
+                <img
+                  src="https://flowbite.s3.amazonaws.com/logo.svg"
+                  className="mr-3 h-8"
+                  alt="GrammyAi Logo"
+                />
+                <span className=" font-mono self-center text-sm md:text-xl font-bold whitespace-nowrap  text-blue-600">
+                  GrammeAI
+                </span>
+              </a>
+              <h1 className="text-center tracking-tighter md:text-sm text-[11px] md:-ml-3 -ml-10 md:mt-3 mt-6">
+                Paraphraser
+              </h1>
+            </div>
+            <div className="flex items-center  lg:order-2">
+              <button
+                type="button"
+                className=" items-center  text-[11.5px] md:text-sm  justify-center text-white bg-blue-600  bg-opacity-95 hover:bg-blue-800 transition duration-300 focus:ring-4 focus:ring-primary-300 font-medium rounded-full px-3 py-2 mr-2 focus:outline-none "
+              >
+                Upgrade to Premium
+              </button>
+
+              <button
+                type="button"
+                onClick={handleUserProfileMenu}
+                className="flex mx-3 text-sm bg-gray-200 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 hover:bg-opacity-80"
+                id="user-menu-button"
+                ariaExpanded="true"
+                dataDropdownoggle="dropdown"
+              >
+                <span className="sr-only">Open user menu</span>
+                <i class="ri-user-fill px-2 py-2.5 rounded-full ri-lg text-gray-600"/>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {showUserProfileMenu&&<UserProfileMenu/>}
+
+    </div>
+  );
+};
+
+export default Navbar;
