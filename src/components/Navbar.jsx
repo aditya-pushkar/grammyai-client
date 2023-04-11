2a810743f823986d4116f6321496d64b27bbd53d
aditya-pushkar <adityapushkar850@gmail.com>
2023-04-12 01:01:16 +0530

    tools sect --added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,77 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-const UserProfileMenu = () => {
+const Tools = () => {
   return (
-    <div className="flex justify-end -mt-2 w-screen">
-<div
-    className="z-50 my-4 w-56  mx-2 text-base list-none bg-white rounded divide-y divide-gray-100 shadow "
-    id="dropdown"
-  >
-    <div className="py-3 px-4">
-      <span className="block text-sm font-semibold text-gray-900">
-        Neil sims
-      </span>
-      <span className="block text-sm font-light text-gray-700 truncate">
-        adi@grammeai.com
-      </span>
-    </div>
-    <ul
-      className="py-1 font-light text-gray-500 dark:text-gray-400"
-      aria-labelledby="dropdown"
-    >
-      <li>
-        <a
-          href="#"
-          className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
-        >
-          My profile
-        </a>
-      </li>
-      
-    </ul>
-    
-    <ul
-      className="py-1 font-light text-gray-500 dark:text-gray-400"
-      aria-labelledby="dropdown"
-    >
-      <li>
-        <a
-          href="#"
-          className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
-        >
-          Sign out
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div className="flex flex-row w-screen h-10  px-10  gap-3  bg-gray-50 overflow-x-auto scrollbar-hide justify-center items-center grow border-b-2">
+      <div className=" flex flex-col  items-center justify-center   cursor-pointer hover:bg-gray-100  px-2">
+      <i class="ri-book-read-line text-blue-600 text-center"></i>
+      <h1 className="text-[12px] font -mt-1 text-gray-700">Paraphraser</h1>
+      </div>
+
+      <div className=" flex flex-col  items-center justify-center   cursor-pointer hover:bg-gray-100  px-2">
+      <i class="ri-edit-2-line text-red-600 text-center"></i>
+      <h1 className="text-[12px] font -mt-1 text-gray-700">Grammar Checker</h1>
+      </div>
     </div>
     
   )
+}
+
+const UserProfileMenu = () => {
+  return (
+    <div className="flex justify-end -mt-2 w-screen absolute">
+      <div
+        className="z-50 my-4 w-56  mx-2 text-base list-none bg-white rounded divide-y divide-gray-100 shadow "
+        id="dropdown"
+      >
+        <div className="py-3 px-4">
+          <span className="block text-sm font-semibold text-gray-900">
+            Neil sims
+          </span>
+          <span className="block text-sm font-light text-gray-700 truncate">
+            adi@grammeai.com
+          </span>
+        </div>
+        <ul
+          className="py-1 font-light text-gray-500 dark:text-gray-400"
+          aria-labelledby="dropdown"
+        >
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
+            >
+              My profile
+            </a>
+          </li>
+        </ul>
+
+        <ul
+          className="py-1 font-light text-gray-500 dark:text-gray-400"
+          aria-labelledby="dropdown"
+        >
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm hover:bg-gray-600 hover:text-gray-50 text-gray-700"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 const Navbar = () => {
-    const [showUserProfileMenu, setShowUserProfileMenu]  = useState(false)
+  const [showUserProfileMenu, setShowUserProfileMenu] = useState(false);
 
-    const handleUserProfileMenu = () => {
-        if(showUserProfileMenu){
-            return setShowUserProfileMenu(false)
-        }
-        return setShowUserProfileMenu(true)
+  const handleUserProfileMenu = () => {
+    if (showUserProfileMenu) {
+      return setShowUserProfileMenu(false);
     }
+    return setShowUserProfileMenu(true);
+  };
   return (
     <div>
       <header>
@@ -94,14 +109,14 @@ const Navbar = () => {
                 dataDropdownoggle="dropdown"
               >
                 <span className="sr-only">Open user menu</span>
-                <i class="ri-user-fill px-2 py-2.5 rounded-full ri-lg text-gray-600"/>
+                <i class="ri-user-fill px-2 py-2.5 rounded-full ri-lg text-gray-600" />
               </button>
             </div>
           </div>
         </nav>
       </header>
-      {showUserProfileMenu&&<UserProfileMenu/>}
-
+      {showUserProfileMenu && <UserProfileMenu />}
+      <Tools/>
     </div>
   );
 };
