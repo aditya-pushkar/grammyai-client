a164cc0fc308d091320ffa4c5d1b9da63ce1345d
aditya-pushkar <adityapushkar850@gmail.com>
2023-04-12 23:51:48 +0530

    grammar-checker page --added

---
+++ src/pages/GrammarChecker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Checker from '../components/grammar-checker/Checker'
+
+const GrammarChecker = () => {
+  return (
+    <div className='mt-20'>
+        <Checker/>
+    </div>
+  )
+}
+
+export default GrammarChecker
